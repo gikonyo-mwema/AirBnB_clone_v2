de89b8074cd93034d45f3d11f672468160109c11
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-19 08:42:10 -0700

    update to all and delete instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {k: v for k, v in FileStorage.__objects.items()
+                    if type(v).__name__ == cls}
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +27,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside"""
+        if obj:
+            obj_ref = obj.to_dict()['__class__'] + '.' + obj.id
+            if obj_ref in self.all():
+                del FileStorage.__objects[obj_ref]
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +55,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
