73547f86a677381199fa1cc67390b91acf03a825
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-19 19:45:41 -0700

    engine done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,56 @@
 #!/usr/bin/python3
+""" DBStorage Module """
 
-class DBStorage
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import BaseModel, Base
+
+
+class DBStorage:
+    """ DBStorage class """
+    __engine = None
+    __session = None
+
+
+    def __init__(self):
+        """ Initialization """
+        self._engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                                      format(getenv('HBNB_MYSQL_USER'),
+                                             getenv('HBNB_MYSQL_PWD'),
+                                             getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Query all objects """
+        result = {}
+        if cls:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                result[key] = obj
+        return result
+
+    def new(self, obj):
+        """ Add object to current database session """
+        self.__session.add(obj)
+
+    def save(self):
+        """ Commit all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Delete  obj from current database session if not None """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ Create all tables in the database and create the current ses """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
