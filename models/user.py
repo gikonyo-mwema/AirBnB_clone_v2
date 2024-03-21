3fb7da3a06bf1b5306ea969e7ec39c444540f10b
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-20 18:31:21 -0700

    minor changes

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.review import Review
+from models.place import Place
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
