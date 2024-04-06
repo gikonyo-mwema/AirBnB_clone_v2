eb7b7a93534ac0c499586de2918d4579f96dc64f
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-06 11:08:39 -0700

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,7 @@ folder and distribute it to web servers.
 """
 
 from fabric.api import local, env, put, run
-from datetime import datetime
-import os
+from os.path import exists
 
 # IPs of your web servers
 env.hosts = ['54.157.151.166', '18.210.33.196']
@@ -18,7 +17,7 @@ def do_deploy(archive_path):
     """
 
     # Check if the file at the path archive_path exists
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
 
     try:
@@ -26,30 +25,33 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         # Uncompress the archive to the folder
-        # /data/web_static/releases on the web server
-        filename = os.path.basename(archive_path)
-        name, ext = os.path.splitext(filename)
-        run('mkdir -p /data/web_static/releases/{}'.format(name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
-            .format(filename, name))
+        # /data/web_static/releases/<archive filename without extension>
+        # on the web server
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            filename, no_ext))
 
-        # Move the contents of the web_static subdir to the parent
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(name, name))
+        # Move the uncompressed files from the web_static subdirectory
+        # to the parent directory
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(no_ext, no_ext))
 
-        # Delete the now empty web_static subdir
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
+        # Delete the web_static subdirectory after moving uncompressed files
+        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
 
         # Delete the archive from the web server
-        run('rm /tmp/{}'.format(filename))
+        run("rm /tmp/{}".format(filename))
 
         # Delete the symbolic link /data/web_static/current from the web server
-        run('rm -rf /data/web_static/current')
+        run("rm -rf /data/web_static/current")
 
-        # Create a symbolic link /data/web_static/current on the web server,
+        # Create a new symbolic link /data/web_static/current on web server,
         # linked to the new version of your code
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(name))
+        # (/data/web_static/releases/<archive filename without extension>)
+        run("ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(no_ext))
 
         return True
     except Exception:
