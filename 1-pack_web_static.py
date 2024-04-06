db316be12c826df8758ed198a64d3abc42a408d5
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-06 13:45:27 +0300

    fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Fabric script create .tgz archive from contents of web_static
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Generate a .tgz archive
+    """
+
+    # Create the version directory if it doesn't exist
+    local('mkdir -p versions')
+
+    # Generate the .tgz archive
+    timestr = datetime.now().strftime('%Y%m%d%H%M%S')
+    path = 'versions/web_static_{}.tgz'.format(timestr)
+
+    result = local('tar -cvzf {} web_static'.format(path))
+
+    # Return the archive path if the archive has beeen generated
+    if result.failed:
+        return None
+    else:
+        return archive_path
