c98b4d6545cec0614d18149dab097c4da1d370de
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-06 11:14:35 -0700

    simplified deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,58 +1,22 @@
 #!/usr/bin/python3
 """
-Fabric script to create a .tgz archive from the contents of the web_static
-folder and distribute it to web servers.
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
 """
 
-from fabric.api import local, env, put, run
-from os.path import exists
-
-# IPs of your web servers
-env.hosts = ['54.157.151.166', '18.210.33.196']
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
 
 
-def do_deploy(archive_path):
-    """
-    Distribute an archive to web servers.
-    """
-
-    # Check if the file at the path archive_path exists
-    if not exists(archive_path):
-        return False
-
+def do_pack():
+    """generates a tgz archive"""
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
-
-        # Uncompress the archive to the folder
-        # /data/web_static/releases/<archive filename without extension>
-        # on the web server
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            filename, no_ext))
-
-        # Move the uncompressed files from the web_static subdirectory
-        # to the parent directory
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(no_ext, no_ext))
-
-        # Delete the web_static subdirectory after moving uncompressed files
-        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
-
-        # Delete the archive from the web server
-        run("rm /tmp/{}".format(filename))
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link /data/web_static/current on web server,
-        # linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
-        run("ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(no_ext))
-
-        return True
-    except Exception:
-        return False
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
