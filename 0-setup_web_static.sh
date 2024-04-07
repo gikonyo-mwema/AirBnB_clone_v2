0838ee2f9e2ff81864e5103fbb5600071d826dd7
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-06 18:40:25 -0700

    search specific line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,7 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '49i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+#sudo sed -i '49i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
