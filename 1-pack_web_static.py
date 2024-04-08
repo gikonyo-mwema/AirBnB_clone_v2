cec8c4ff51b64d1fb7b5ea5cb34cee229fe44c2c
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-08 04:23:34 +0300

    improved output to include bytes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@ Fabric script create .tgz archive from contents of web_static
 
 from fabric.api import local
 from datetime import datetime
-
+import os
 
 def do_pack():
     """
@@ -15,17 +15,24 @@ def do_pack():
     # Create the version directory if it doesn't exist
     local('mkdir -p versions')
 
-    # Generate the .tgz archive
+    # Generate the current time 
     timestr = datetime.now().strftime('%Y%m%d%H%M%S')
-    path = 'versions/web_static_{}.tgz'.format(timestr)
 
-    result = local('tar -cvzf {} web_static'.format(path))
+    # Create the archive
+    result = local(f'tar -cvzf versions/web_static_{timestr}.tgz web_static')
 
-    # Print the stderr of the tar command
-    print(result.stderr)
+    path = f'versions/web_static_{timestr}.tgz'
 
-    # Return the archive path if the archive has beeen generated
+    # Check if the file exists before getting its size
+    if os.path.exists(path):
+        size = os.path.getsize(path)
+        print(f"web_static packed: {path} -> {size}Bytes")
+    else:
+        print(f"Failed to create the archive at {path}")
+
+    # Return the archive path if the archive has been generated
     if result.failed:
         return None
     else:
         return path
+
