39a79f7052c5a65e0c2034254ce6c0c009b105a9
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-20 04:38:07 +0300

    update to add close methods

--- models/state.py
+++ models/state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Defines the State class. """
+"""
+Defines the State class.
+"""
 import models
 from models.base_model import Base
 from models.base_model import BaseModel
@@ -10,7 +12,13 @@ from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """
+    The State class inherits from BaseModel and Base. It links to the MySQL
+    table 'states', and it has the class attribute 'name' which represents a
+    string of maximum 128 characters and cannot be null. It also has the
+    'cities' relationship which represents a one-to-many relationship between
+    the State and City classes.
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
@@ -19,10 +27,9 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             """
-            getter attribute cities that returns the list of Cities
+            Returns the list of City instances with state_id equals to the
+            current State.id. This property method gets invoked only if the
+            environment variable HBNB_TYPE_STORAGE is not set to "db".
             """
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
