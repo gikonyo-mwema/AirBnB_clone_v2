cf2d8359bf1cff27d17799fb7949b38c54308aa3
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 13:06:44 +0300

    404 -removal

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,30 +7,16 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states')
-def states():
-    """
-    This function retrieves all states from storage
-    and renders them in an HTML page
-    """
-    states = sorted(storage.all("State").value(),
-                    key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/states/<id>')
-def states_id(id):
-    """
-    This function retrieves a state by its id from storage
-    and renders it in an HTML page
-    """
-    state = storage.get("State", id)
-    if state is None:
-        return render_template('404.html'), 404
-    return render_template('9-states.html', state=state)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
