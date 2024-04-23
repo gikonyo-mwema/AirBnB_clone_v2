44090589d783f359aa1934a47cdbdcb295740ee7
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 11:20:58 +0300

    changes to state call

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,17 +5,20 @@ Module starts a Flask web application
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list")
 def states_list():
     """
     This function renders a HTML page with a list of all State
     objects present in DBStorage sorted by name (A->Z)
     """
-    states = storage.all("State")
+    states = sorted(list(storage.all("State").values()),
+                    key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
@@ -27,5 +30,5 @@ def handle_teardown(exc):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
