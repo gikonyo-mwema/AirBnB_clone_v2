e816739120b2bbe036c551adcdbcd3d96ff3b099
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 12:23:09 +0300

    single quotes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,13 +5,13 @@ Module starts a Flask web application
 
 from flask import Flask, render_template
 from models import storage
+from models import *
 from models.state import State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     """
     This function renders a HTML page with a list of all State
@@ -23,12 +23,12 @@ def states_list():
 
 
 @app.teardown_appcontext
-def handle_teardown(exc):
+def handle_teardown(exception):
     """
     This function removes the current SQLAlchemy session
     """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
