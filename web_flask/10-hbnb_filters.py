883e59e85965b2874d29e168f206893e2abe32d5
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 17:29:42 +0300

    10 html

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -7,22 +7,18 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb_filters')
+@app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """
     This function retrieves all states, cities and amenities from storage
     and renders them in an HTML page
     """
-    states = sorted(storage.all("State").values(),
-                    key=lambda state: state.name)
-    cities = sorted(storage.all("City").value(), key=lambda city: city.name)
-    amenities = sorted(storage.all("Amenity").value(),
-                       key=lambda amenity: amenity.name)
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
     return render_template('10-hbnb_filters.html', states=states,
-                           cities=cities, amenities=amenities)
+                           amenities=amenities)
 
 
 @app.teardown_appcontext
